5b337c64822bb35b86271afe06b7f31cab1d440c
David Howes <dthowes@umich.edu>
2021-10-31 16:50:55 -0400

    Add files via upload
    Bug fix, needed security group passed in.

--- main.tf
+++ main.tf
@@ -28,14 +28,14 @@ data "aws_ami" "centos" {
 }
 
 data "template_file" "user_data" {
-  template = file("cloudinit.yaml")
+  template = file("scripts/cloudinit.yaml")
 }
 
 resource "aws_instance" "minecraft" {
   ami = data.aws_ami.centos.id
   instance_type = "t2.medium"
   subnet_id = data.terraform_remote_state.vpc.outputs.vpc_id
-  vpc_security_group_ids = [aws_security_group.sg_minecraft.id]
+  vpc_security_group_ids = [var.sg_id]
   associate_public_ip_address = true
   user_data = data.template_file.user_data.rendered
   
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "environment_tag" {
   description = "Tag for Dev, QA, or Prod environment"
 }
+
+variable "sg_id" {
+  description = "Security group to be used"
+}
